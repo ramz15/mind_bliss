03e268703ef1f93995a8081eb07a49eaa6878e82
Karl Gusner <karlgusner@gmail.com>
2013-10-14 17:23:00 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+MindBliss::Application.config.secret_key_base = '2db0d6ea5aa6256d94c23f3ed22baffe05c20b7503fc70249d9bfea51649ff068556a85fbcb0bcd46c372ee175ac0a59d7430a9e8dc5eea0e425b2b159860d0c'
